729e249657fa081dccf35fd8503a91e2d37f340e
Toysin <toysin1203@hotmail.com>
2021-12-29 17:24:38 +0900

    0x02

--- Practice/0x02/2309.cpp
+++ Practice/0x02/2309.cpp
@@ -3,30 +3,38 @@ using namespace std;
 
 int arr[9], result[7], total;
 
-int is_valid(int v, int cnt) {
-	for (int i = 0; i < cnt; i++)
-		if (v == result[i])
-			return (0);
-	return (1);
-}
+// int is_valid(int v, int cnt) {
+// 	for (int i = 0; i < cnt; i++)
+// 		if (v == result[i])
+// 			return (0);
+// 	return (1);
+// }
 
-int find_seven(int cnt) {
-	if (cnt == 7) {
-		total = 0;
-		for (int i = 0; i < 7; i++)
-			total += result[i];
-		if (total == 100) {
-			for (int i = 0; i < 7; i++)
-				cout << result[i] << "\n";
-			return (1);
-		}
-		return (0);
-	}
+// int find_seven(int cnt) {
+// 	if (cnt == 7) {
+// 		total = 0;
+// 		for (int i = 0; i < 7; i++)
+// 			total += result[i];
+// 		if (total == 100) {
+// 			for (int i = 0; i < 7; i++)
+// 				cout << result[i] << "\n";
+// 			return (1);
+// 		}
+// 		return (0);
+// 	}
+// 	for (int i = 0; i < 9; i++) {
+// 		result[cnt] = arr[i];
+// 		if (is_valid(arr[i], cnt))
+// 			if (find_seven(cnt + 1))
+// 				return (1);
+// 	}
+// }
+
+void print_except(int a, int b)
+{
 	for (int i = 0; i < 9; i++) {
-		result[cnt] = arr[i];
-		if (is_valid(arr[i], cnt))
-			if (find_seven(cnt + 1))
-				return (1);
+		if (i == a || i == b) continue;
+		cout << arr[i] << "\n";
 	}
 }
 
@@ -34,8 +42,17 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	for (int i = 0; i < 9; i++) cin >> arr[i];
+	for (int i = 0; i < 9; i++) {
+		cin >> arr[i];
+		total += arr[i];
+	}
 	sort(arr, arr + 9);
-
-	find_seven(0);
+	for (int i = 0; i < 8; i++) {
+		for (int j = i + 1; j < 9; j++) {
+			if (total - arr[i] - arr[j] == 100) {
+				print_except(i, j);
+				return 0;
+			}
+		}
+	}
 }
