67d5cc4925706552463db8ba3897187e17e0425d
Toysin <toysin1203@hotmail.com>
2021-12-29 17:27:36 +0900

    2309 recursive ver

--- Practice/0x02/2309.cpp
+++ Practice/0x02/2309.cpp
@@ -3,32 +3,33 @@ using namespace std;
 
 int arr[9], result[7], total;
 
-// int is_valid(int v, int cnt) {
-// 	for (int i = 0; i < cnt; i++)
-// 		if (v == result[i])
-// 			return (0);
-// 	return (1);
-// }
+int is_valid(int v, int cnt) {
+	for (int i = 0; i < cnt; i++)
+		if (v == result[i])
+			return (0);
+	return (1);
+}
 
-// int find_seven(int cnt) {
-// 	if (cnt == 7) {
-// 		total = 0;
-// 		for (int i = 0; i < 7; i++)
-// 			total += result[i];
-// 		if (total == 100) {
-// 			for (int i = 0; i < 7; i++)
-// 				cout << result[i] << "\n";
-// 			return (1);
-// 		}
-// 		return (0);
-// 	}
-// 	for (int i = 0; i < 9; i++) {
-// 		result[cnt] = arr[i];
-// 		if (is_valid(arr[i], cnt))
-// 			if (find_seven(cnt + 1))
-// 				return (1);
-// 	}
-// }
+int find_seven(int cnt) {
+	if (cnt == 7) {
+		total = 0;
+		for (int i = 0; i < 7; i++)
+			total += result[i];
+		if (total == 100) {
+			for (int i = 0; i < 7; i++)
+				cout << result[i] << "\n";
+			return (1);
+		}
+		return (0);
+	}
+	for (int i = 0; i < 9; i++) {
+		result[cnt] = arr[i];
+		if (is_valid(arr[i], cnt))
+			if (find_seven(cnt + 1))
+				return (1);
+	}
+	return (0);
+}
 
 void print_except(int a, int b)
 {
