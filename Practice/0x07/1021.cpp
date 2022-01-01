82fa04b6c40152f157a264383c05749667c89bf8
Toysin <toysin1203@hotmail.com>
2022-01-01 15:49:37 +0900

    0x07 Done

--- Practice/0x07/1021.cpp
+++ Practice/0x07/1021.cpp
@@ -5,12 +5,26 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int N, M;
-	int delta = 0;
+	int N, M, ans = 0;
+	deque<int> DQ;
 	cin >> N >> M;
+	for (int i = 1 ; i <= N; i++) DQ.push_back(i);
 	while (M--) {
 		int n;
 		cin >> n;
-
+		int idx = find(DQ.begin(), DQ.end(), n) - DQ.begin();
+		while (DQ.front() != n) {
+			if (idx < DQ.size() - idx) {
+				DQ.push_back(DQ.front());
+				DQ.pop_front();
+			}
+			else {
+				DQ.push_front(DQ.back());
+				DQ.pop_back();
+			}
+			ans++;
+		}
+		DQ.pop_front();
 	}
+	cout << ans;
 }
